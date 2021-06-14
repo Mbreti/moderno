3cea6a2184e6ab5f5181b9b86272d42cc40fa354
Mbreti <61512014+Mbreti@users.noreply.github.com>
2021-06-14 23:25:59 +0500

    added section product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(function () {
-    var mixer = mixitup(".products__inner-box");
+    const mixer = mixitup(".products__inner-box");
+
+    $(".rate-start").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true,
+    });
+
+    $(".product-slider__inner").slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    });
 });
